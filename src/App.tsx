8dc660aff5cd8a22bdf7ead602f5abf5531ffead
Ivan Gubkin <iv.gubkin19988@gmail.com>
2024-10-07 00:23:39 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from './components/ui/button'
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <Button className="bg-zinc-950 dark:bg-white">Button</Button>
+    </>
+  );
+}
+
+export default App
