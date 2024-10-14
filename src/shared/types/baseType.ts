7723a33afde1fe99774803ca0600e238b3e5fc99
Ivan Gubkin <iv.gubkin19988@gmail.com>
2024-10-14 20:28:31 +0300

    fix register form

--- src/shared/types/baseType.ts
+++ src/shared/types/baseType.ts
@@ -2,3 +2,14 @@ export type LoginType = {
   username: string;
   password: string;
 };
+
+export type RegisterType = {
+  name: string;
+  description: string;
+  owner_name: string;
+  owner_email: string;
+  owner_phone: string;
+  properties?: {[key: string]: string};
+  password: string;
+  pincode: string;
+};
--- src/widgets/register/ui/register.tsx
+++ src/widgets/register/ui/register.tsx
@@ -1,67 +1,85 @@
+import { RegisterType } from "@/shared/types/baseType";
 import {Button} from "@/shared/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/shared/ui/card";
 import {Input} from "@/shared/ui/input";
-import {Tabs, TabsContent} from "@/shared/ui/tabs";
+
 import {useForm} from "react-hook-form";
 
-type RegisterType = {
-  name: string;
-  description: string;
-  owner_name: string;
-  owner_email: string;
-  owner_phone: string;
-  properties?: {[key: string]: string};
-  password: string;
-  pincode: string;
-};
+
 
 export function RegisterWidgets() {
-  const {register, handleSubmit} = useForm<RegisterType>();
+  const {
+    register,
+    handleSubmit,
+    formState:{errors}
+    
+  } = useForm<RegisterType>();
 
   const onSubmit = (data: RegisterType): void => {
     console.log(data);
   };
+  console.log(errors);
+  
+ 
+ 
 
   return (
-    <Tabs
-      defaultValue="register"
-      className="flex justify-center items-center fixed h-full w-full"
-    >
-      <TabsContent value="register">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Input
-            placeholder="Название организации"
-            {...register("name", {required: "Обязательное поле"})}
-          />
-          <Input
-            placeholder="Ваше имя"
-            {...register("owner_name", {required: "Обязательное поле"})}
-          />
-          <Input
-            placeholder="Ваш телефон"
-            {...register("owner_phone", {required: "Обязательное поле"})}
-          />
-          <Input
-            placeholder="Email"
-            {...register("owner_email", {required: "Обязательное поле"})}
-          />
-          <Input
-            placeholder="Комментарий"
-            {...register("description", {required: "Обязательное поле"})}
-          />
-          <Input
-            placeholder="Пароль"
-            {...register("password", {
-              required: "Обязательное поле",
-              minLength: 6,
-            })}
-          />
-          <Input
-            placeholder="Pin с email"
-            {...register("pincode", {required: "Обязательное поле"})}
-          />
-          <Button variant={"secondary"}>Регистрация</Button>
-        </form>
-      </TabsContent>
-    </Tabs>
+    <div className="grid place-content-center h-[88vh]">
+      <Card className="w-[550px]">
+        <CardHeader>
+          <CardTitle>Регистариция СRM</CardTitle>
+          <CardDescription></CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-3"
+          >
+            <Input
+              placeholder="Название организации"
+              {...register("name", {required: "Обязательное поле"})}
+            />
+            <Input
+              placeholder="Ваше имя"
+              {...register("owner_name", {required: "Обязательное поле"})}
+            />
+            <Input
+              placeholder="Ваш телефон"
+              {...register("owner_phone", {required: "Обязательное поле"})}
+            />
+            <div className="flex gap-1">
+              <Input
+                placeholder="Email"
+                {...register("owner_email", {required: "Обязательное поле"})}
+              />
+              <Button disabled={false} onClick={()=>{console.log(1);
+              }} type="button" >Отправить код</Button>
+            </div>
+            <Input
+              placeholder="Комментарий"
+              {...register("description", {required: "Обязательное поле"})}
+            />
+            <Input
+              placeholder="Пароль"
+              {...register("password", {
+                required: "Обязательное поле",
+                minLength: 6,
+              })}
+            />
+            <Input
+              placeholder="Pin с email"
+              {...register("pincode", {required: "Обязательное поле"})}
+            />
+            <Button type="submit">Регистрация</Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
